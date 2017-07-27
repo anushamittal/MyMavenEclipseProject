607651088735b2ac1be8964d9cff17628da327c2
Anusha Mittal Biswas <anushamittal0884@gmail.com>
2017-07-27 21:59:34 +1200

    This is the first eclipse project to be committed to Git repository. We should avoid the important files like project, classpath, pom because it contains imp info. But i am committing because only i will be using this project

---
+++ src/test/java/com/learnMaven/selenium/FirstMavenonJenkins.java
@@ -0,0 +1,11 @@
+package com.learnMaven.selenium;
+
+import org.testng.annotations.Test;
+
+public class FirstMavenonJenkins {
+
+	@Test
+	public void firstmethod(){
+		System.out.println("This is the first method and the class to be deployed on jenkins - A maven project");
+	}
+}
