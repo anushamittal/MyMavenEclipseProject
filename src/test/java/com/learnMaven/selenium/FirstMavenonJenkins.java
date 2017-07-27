e54628ca7440b6f9c64a4e04f81650f88312cc34
Anusha Mittal Biswas <anushamittal0884@gmail.com>
2017-07-27 22:49:10 +1200

    4th commit

--- src/test/java/com/learnMaven/selenium/FirstMavenonJenkins.java
+++ src/test/java/com/learnMaven/selenium/FirstMavenonJenkins.java
@@ -9,5 +9,8 @@ public class FirstMavenonJenkins {
 		System.out.println("This is the first method and the class to be deployed on jenkins - A maven project");
 		System.out.println("This is the second commit and wil be shown only wheni commit the changes");
 		System.out.println("This is my third commit");
+		System.out.println("This is my fourth commit for git and jenkins");
 	}
 }
+
+//refer to https://www.youtube.com/watch?v=BH4OqYHoHC0 for details
