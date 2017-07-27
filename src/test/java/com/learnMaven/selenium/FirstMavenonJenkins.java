82be2bc82541d676e27bc980ae81f3b4689700bd
Anusha Mittal Biswas <anushamittal0884@gmail.com>
2017-07-27 22:26:09 +1200

    second commit

--- src/test/java/com/learnMaven/selenium/FirstMavenonJenkins.java
+++ src/test/java/com/learnMaven/selenium/FirstMavenonJenkins.java
@@ -7,5 +7,6 @@ public class FirstMavenonJenkins {
 	@Test
 	public void firstmethod(){
 		System.out.println("This is the first method and the class to be deployed on jenkins - A maven project");
+		System.out.println("This is the second commit and wil be shown only wheni commit the changes");
 	}
 }
